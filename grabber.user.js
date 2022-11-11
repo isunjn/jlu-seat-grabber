96ee4966fa34b6f13c5d0e8cbb9e4dadb9ad50a5
Jianan Sun <isunjn@gmail.com>
2022-11-11 16:06:22 +0800

    v0.3

--- grabber.user.js
+++ grabber.user.js
@@ -2,40 +2,63 @@
 // @name        JLU Seat Grabber
 // @namespace   jlu-scripting
 // @match       http://libzwyy.jlu.edu.cn/*
-// @version     0.1
+// @version     0.3
 // @description 吉林大学鼎新图书馆自动抢座
 // @author      anonymous
 // @run-at      document-end
-// @grant       none
+// @grant       GM_info
 // ==/UserScript==
+
 
 /* ---------------- configuration ----------------- */
 
 const RESV_URL = 'http://libzwyy.jlu.edu.cn/ic-web/reserve';
-
-const tomorrow = fmtDate(new Date(Date.now() + 24 * 60 * 60 * 1000));
-const configMemo = localStorage.getItem('GRABBER_CONFIG');
-const config = configMemo ? JSON.parse(configMemo) : {
+const REQUEST_LIMIT = 100; /* 别瞎改，请求量太大会造成服务器宕机，100已经很高了 */
+const COUNT_DOWN_INTERVAL =100;
+const SEAT_ID_BASE = {
+    '2B': 100652811,
+    '3A': 100653165,
+    '3B': 100653289,
+    '4A': 100653477,
+    '4B': 100653649,
+    '5A': 100653893,
+    '5B': 100654024,
+};
+const DEFAULT_TIMING_CONTROL = {
+    hit: 2,
+    interval: 100,
+    timeout: 1,
+};
+const DEFAULT_POLLING_CONTROL = {
+    hit: 1,
+    interval: 1000,
+    timeout: 30,
+};
+const DEFAULT_CONFIG = {
     seats: '',
-    date: tomorrow,
+    date: '',
     beginTime: '07:00',
     endTime: '22:00',
     mode: 'timing', // 'timing' | 'polling'
     aheadTime: 30,
     targetTime: '21:00',
-    hit: 5,
-    interval: 100,
-    timeout: 2,
+    ...DEFAULT_TIMING_CONTROL,
 };
-config.date = tomorrow;
+const STORAGE_KEY = 'GRABBER_CONFIG';
+const RE_SEAT_ONE = /^(2B|3A|3B|4A|4B|5A|5B)(\d{3})$/i;
+const RE_SEAT_RANGE = /^(2B|3A|3B|4A|4B|5A|5B)(\d{3})-(\d{3})$/i;
+
+const storedCfg = localStorage.getItem(STORAGE_KEY);
+const config = storedCfg ? JSON.parse(storedCfg) : DEFAULT_CONFIG;
+config.date = fmtDate(new Date(Date.now() + 24 * 60 * 60 * 1000));
 
 /* ---------------- init grabber ----------------- */
 
 const html = `
-<div id="grabber">
+<div id="grabber" class="hidden">
 <!-- bar section -->
 <div id="bar" class="flex-container">
-  <div id="title">JLU Seat Grabber v0.1</div>
+  <div id="title">JLU Seat Grabber v${GM_info.script.version}</div>
   <div id="meta-btns-wrapper" class="flex-container">
     <div class="meta-btn" id="meta-btn-help">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
@@ -136,8 +159,27 @@ const html = `
 
 <!-- help section -->
 <div id="manual" class="hidden">
-  <p>使用说明/注意事项：</p>
-  <p>- 鼎新图书馆自动抢座脚本</p>
+  <p>使用说明 / 注意事项：</p>
+  <p> - 请使用最新版Edge/Chrome浏览器，快捷键‘Ctrl+空格’显示/隐藏界面</p>
+  <p> - 最好可以关闭电脑上其他程序/浏览器标签页，插上电源并打开电脑高性能模式</p>
+  <p> - 两种模式：‘定时抢座’在‘定时目标’的前‘提前’秒开始请求，‘轮询捡漏’直接开始</p>
+  <p> - 请求逻辑：每‘间隔’毫秒执行‘连击’次发送，每次发送对所选座位依次请求1次</p>
+  <p> - 保持‘连击’是一个较小的数字。请求共持续‘超时’分钟，若超时后仍未抢到则停止尝试</p>
+  <p> - 目前(2022.11)服务器时间比标准时间快18秒左右，建议‘提前’设置为20~30</p>
+  <p> - 座位支持两种写法：单独一个例如2B001，一个范围例如3A001-100，升降序均可</p>
+  <p> - 座位字符串可以写多个，不同的字符串之间用空格或回车隔开，大小写不敏感</p>
+  <p> - 你需要自行检查某座位号是否存在/开放，否则发送无效请求成功率会降低，注意座位号不一定连续</p>
+  <p> - 卡点抢第二天座位时最好定时抢，成功率更高，别提前好多分钟就开始轮询，没必要</p>
+  <p> - 定时卡点抢时‘座位数’*‘连击’保持在5以下，‘间隔’可以设为200/100，抢某一个或几个座位</p>
+  <p> - 轮询捡漏时‘座位’设多些，‘间隔’长一点，‘连击’设为1即可，例如200个座位每1s/2s请求1次</p>
+  <p> - 请求频率别太高(例如超过100/s)，浏览器对请求队列有一定资源限制，资源不足后请求会失败</p>
+  <p> - 过多过快的请求可能造成服务器宕机，同时用脚本的人过多性质可能就变成DDoS攻击了</p>
+  <p> - 如果不是特别需要，最好别捡漏那些大半夜换时间的人的座位，做事不要太过分</p>
+  <p> - 偷摸用，把握好分寸，别给服务器造成太大压力，用的人越少成功率越高，也越不容易被学校发现</p>
+  <p> - 切勿传播本脚本，切勿用售卖、代抢等方式牟利，否则可能涉嫌刑法‘破坏计算机信息系统罪’</p>
+  <p>免责声明：</p>
+  <p> - 本脚本仅供个人学习研究之用，不使用任何破坏计算机信息系统的技术，不修改任何数据</p>
+  <p> - 使用脚本造成的任何后果均由使用者本人全部承担，属于个人行为，与开发者无关</p>
 </div>
 </div>
 `;
@@ -156,13 +198,17 @@ const css = `
   #grabber * {
     font-size: 10px;
   }
+  #grabber option {
+    font-size: 16px;
+  }
   #grabber {
     box-sizing: border-box;
     margin: 0;
     width: 600px;
     position: fixed;
     top: 1em;
-    left: 1em;
+    left: 50vw;
+    transform: translateX(-50%);
     z-index: 9999;
     background-color: var(--bg-color);
     color: var(--fg-color);
@@ -172,6 +218,9 @@ const css = `
     flex-direction: column;
     gap: 0.5em;
     box-shadow: var(--btn-color) 0px 0px 0px 3px;
+  }
+  #grabber.hidden {
+    display: none;
   }
   #grabber #bar {
     margin-bottom: 1em;
@@ -294,6 +343,9 @@ const css = `
   #grabber #manual.hidden {
     display: none;
   }
+  #grabber #manual p {
+    margin: 0.2em 0;
+  }
 `;
 
 const style = document.createElement('style');
@@ -359,6 +411,7 @@ const option = {
 grabber.addEventListener('change', handleInputChange);
 btnOperate.addEventListener('click', handleOperate);
 btnHelp.addEventListener('click', handleToggleHelp);
+document.addEventListener('keydown', handleToggleDisplay);
 makeGrabberDragable();
 
 function handleInputChange(evt) {
@@ -371,13 +424,9 @@ function handleInputChange(evt) {
         case 'i-mode': {
             config.mode = val;
             timingConfigElements.forEach(el => el.disabled = val == 'timing' ? false : true);
-            const [hit, interval, timeout] = val == 'timing' ? [5, 100, 2] : [1, 1000, 30];
-            config.hit = hit;
-            config.interval = interval;
-            config.timeout = timeout;
-            elHit.value = hit;
-            elInterval.value = interval;
-            elTimeout.value = timeout;
+            const { hit, interval, timeout } = val == 'timing' ? DEFAULT_TIMING_CONTROL : DEFAULT_POLLING_CONTROL;
+            [config.hit, config.interval, config.timeout] = [hit, interval, timeout];
+            [elHit.value, elInterval.value, elTimeout.value] = [hit, interval, timeout];
             break;
         }
         case 'i-aheadTime': config.aheadTime = parseFloat(val); break;
@@ -387,12 +436,21 @@ function handleInputChange(evt) {
         case 'i-timeout': config.timeout = parseFloat(val); break;
         default: throw 'unreachable';
     }
-    localStorage.setItem('GRABBER_CONFIG', JSON.stringify(config));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(config));
 }
 
 function handleOperate() {
     console.log(config);
-    if (validate()) startRuning();
+    const result = validate();
+    switch (result.type) {
+        case 'Ok': startRuning(); break;
+        case 'Err': {
+            state.errors = result.error;
+            state.status = 'erroring';
+            updateUI();
+            break;
+        }
+    }
 }
 
 function handleCancel() {
@@ -416,6 +474,13 @@ function handleCancel() {
 
 function handleToggleHelp() {
     elManual.classList.toggle('hidden');
+}
+
+function handleToggleDisplay(evt) {
+    if (evt.ctrlKey && evt.key == ' ') {
+        evt.preventDefault();
+        grabber.classList.toggle('hidden');
+    }
 }
 
 function makeGrabberDragable() {
@@ -504,81 +569,74 @@ function updateResquestCountUI() {
     state.countElement.innerText = state.requestCount;
 }
 
-/* ---------------------- parse and validate ---------------------- */
+/* ---------------------- validation ---------------------- */
 
 function validate() {
     const errors = [];
 
-    const login = JSON.parse(sessionStorage.getItem('isLogin') || 'false');
-    if (!login) {
-        errors.push('用户未登录');
-    } else {
-        state.user = JSON.parse(sessionStorage.getItem('userInfo'));
+    let result;
+    result = checkUserLogin();
+    switch (result.type) {
+        case 'Ok': state.user = result.value; break;
+        case 'Err': errors.push(...result.error); break;
     }
-
-    const result = parseTargetSeats();
+    result = parseTargetSeats();
     switch (result.type) {
         case 'Ok': state.targetSeats = result.value; break;
         case 'Err': errors.push(...result.error); break;
     }
+    result = checkConfig();
+    switch (result.type) {
+        case 'Ok': break;
+        case 'Err': errors.push(...result.error); break;
+    }
 
+    return errors.length  > 0 ? Err(errors) : Ok();
+}
+
+function checkUserLogin() {
+    const login = JSON.parse(sessionStorage.getItem('isLogin') || 'false');
+    const user = login ? JSON.parse(sessionStorage.getItem('userInfo')) : null;
+    return login ? Ok(user) : Err(['未登录']);
+}
+
+function checkConfig() {
+    const errors = [];
+    if (config.seats.trim() == '') errors.push('未指定座位号');
     for (const [k, v] of Object.entries(config)) {
-        if (v !== 0 && !v && k != 'seats') errors.push(`配置项 ${k} 不能为空`);
+        if (k != 'seats' && v !== 0 && !v) errors.push(`配置项 ${k} 不能为空`);
     }
-
-    if (config.interval && config.hit && state.targetSeats.length) {
-        const qps = Math.floor((1000 / config.interval) * config.hit * state.targetSeats.length);
-        if (qps > 200) errors.push(`请求频率最好别超过 200 , 当前为 ${qps}`);
+    const [hit, interval, seatCount] = [config.hit, config.interval, state.targetSeats.length];
+    if (hit && interval && seatCount) {
+        const freq = Math.ceil((1000 / interval) * hit * seatCount);
+        if (freq > REQUEST_LIMIT) {
+            errors.push(`请求频率最好别超过 ${REQUEST_LIMIT} , 当前为 1000 / ${interval} * ${hit} * ${seatCount} = ${freq}`);
+        }
     }
-
-    state.errors = errors;
-    if (errors.length > 0) {
-        state.status = 'erroring';
-        updateUI();
-        return false;
-    }
-    return true;
+    return errors.length > 0 ? Err(errors) : Ok();
 }
 
 function parseTargetSeats() {
-    const BASE_MAP = {
-        '2B': 100652811,
-        '3A': 100653165,
-        '3B': 100653289,
-        '4A': 100653477,
-        '4B': 100653649,
-        '5A': 100653893,
-        '5B': 100654024,
-    };
-    const RE_ONE_SEAT = /^(2B|3A|3B|4A|4B|5A|5B)(\d{3})$/i;
-    const RE_SEAT_SEQUENCE = /^(2B|3A|3B|4A|4B|5A|5B)(\d{3})-(\d{3})$/i;
-
-    const seats = [];
-    const errors = [];
+    const seats = [], errors = [];
     for (const str of (config.seats.match(/\S+/g) || [])) {
         let match;
-        if (match = str.match(RE_ONE_SEAT)) {
+        if (match = str.match(RE_SEAT_ONE)) {
             const [, space, num] = match;
-            seats.push(BASE_MAP[space.toUpperCase()] + parseInt(num));
-        } else if (match = str.match(RE_SEAT_SEQUENCE)) {
-            const [, space, start, end] = match;
-            const base = BASE_MAP[space.toUpperCase()];
-            for (let num = parseInt(start); num <= parseInt(end); num++) {
+            seats.push(SEAT_ID_BASE[space.toUpperCase()] + parseInt(num));
+        } else if (match = str.match(RE_SEAT_RANGE)) {
+            const [, space, firstNum, secondNum] = match;
+            const base = SEAT_ID_BASE[space.toUpperCase()];
+            const start = parseInt(firstNum);
+            const end = parseInt(secondNum);
+            const step = start < end ? 1 : -1;
+            for (let num = start; num != (end + step); num += step) {
                 seats.push(base + num);
             }
         } else {
             errors.push(`无效的座位号：${str}`);
         }
     }
-    if (seats.length == 0 && errors.length == 0) {
-        errors.push('未指定座位号');
-    }
-
-    if (errors.length > 0) {
-        return { type: 'Err', error: errors };
-    } else {
-        return { type: 'Ok', value: dedup(seats) };
-    }
+    return errors.length > 0 ? Err(errors) : Ok(dedup(seats));
 }
 
 /* ----------------------------- core logic -------------------------------- */
@@ -589,11 +647,9 @@ function startRuning() {
     payload.resvBeginTime = `${config.date} ${config.beginTime}:00`;
     payload.resvEndTime = `${config.date} ${config.endTime}:00`;
     option.headers.token = state.user.token;
-
     switch (config.mode) {
         case 'timing': timingGrab(); break;
         case 'polling': pollingGrab(); break;
-        default: throw 'unreachable';
     }
 }
 
@@ -610,18 +666,18 @@ function timingGrab() {
         } else {
             state.countDown = Math.ceil(distanceMs / 1000);
             updateCountDownUI();
-            state.checkingTimer = setTimeout(checkTime, 100);
+            state.checkingTimer = setTimeout(checkTime, COUNT_DOWN_INTERVAL);
         }
     }
     checkTime();
 }
 
 function pollingGrab() {
-      state.status = 'grabing';
-      updateUI();
-      startGiveUpTimeout();
-      grab();
-      state.grabingTimer = setInterval(grab, config.interval);
+    state.status = 'grabing';
+    updateUI();
+    startGiveUpTimeout();
+    grab();
+    state.grabingTimer = setInterval(grab, config.interval);
 }
 
 function startGiveUpTimeout() {
@@ -641,36 +697,43 @@ function grab() {
         for (const seat of seats) {
             payload.resvDev[0] = seat;
             option.body = JSON.stringify(payload);
-            fetch(RESV_URL, option)
-              .then(parseResponse)
-              .then(checkGrabResult)
-              .catch(ignoreError);
+            fetch(RESV_URL, option).then(parseResponse).then(checkFetchResult).catch(checkFetchError);
         }
     }
     state.requestCount += seats.length * hit;
     updateResquestCountUI();
 }
 
-function ignoreError() {}
-
 function parseResponse(resp) {
     return resp.json();
 }
 
-function checkGrabResult(result) {
+function checkFetchResult(result) {
     if (result.code == 0) {
         clearInterval(state.grabingTimer);
         state.status = 'success';
         state.successData = result.data;
         updateUI();
-    } else {
-        if (result.message.endsWith('当前时段有预约') && state.status == 'grabing') {
-            clearInterval(state.grabingTimer);
-            state.status = 'fail';
-            state.failMsg = '你当前所选时段已有预约';
-            updateUI();
+    } else if (state.status == 'grabing') {
+        if (result.message.endsWith('当前时段有预约')) {
+            failWith('你当前所选时段已有预约');
+        } else if (result.message == '用户未登录，请重新登录') {
+            failWith('登录超时');
         }
     }
+}
+
+function checkFetchError(err) {
+    if (err.message == 'Failed to fetch') {
+        failWith('网络错误，可能是请求频率过高');
+    }
+}
+
+function failWith(msg) {
+    clearInterval(state.grabingTimer);
+    state.status = 'fail';
+    state.failMsg = msg;
+    updateUI();
 }
 
 /* ---------------------- utils ---------------------- */
@@ -690,4 +753,12 @@ function fmtTime(dt) {
 
 function dedup(arr) {
     return Array.from(new Set(arr));
+}
+
+function Ok(value) {
+    return { type: 'Ok', value };
+}
+
+function Err(error) {
+    return { type: 'Err', error };
 }
